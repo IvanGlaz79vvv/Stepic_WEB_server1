569f00ecd3c41dc1d2fc4a9c5a4ff812fbbe62be
IvanTest-31 <ivangl79@gmail.com>
2023-12-16 05:39:43 +0300

    Перед 1.4  Java: Запуск сервера

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), 'authform');
+
+        server.start();
+        server.join();
     }
 }
